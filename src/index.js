9f4fb04e06ee86b05bdc8af6ffce1dfc1b0b40dd
NickW16 <171855220+NickW16@users.noreply.github.com>
2025-03-18 11:57:02 -0300

    add theme changer to the page

--- src/index.js
+++ src/index.js
@@ -16,7 +16,18 @@ function renderMainPageMessage (){
                                 </ul>
                                 <p>You can also add a description and some extra notes to the task.</p>
                                 <p>And, on top of that, if you ever need to change anything you can simply click the <i>Edit Task</i> button and update your task info!</p>
-                                <p>This is a project made for The Odin Project course, by <a href="https://github.com/NickW16" style="color: lightgreen">NickW16</a>.</p>`;                       
+                                
+                                <p>Change the website's theme to your preference!</p>
+                                <select id="theme-select">
+                                    <option value="light">Light</option>
+                                    <option value="dark">Dark</option>
+                                    <option value="blue">Blue</option>
+                                    <option value="pink">Pink</option>
+                                    <option value="red">Red</option>
+                                </select>
+                                <p style="margin-top: 50px">This is a project made for The Odin Project course, by <a href="https://github.com/NickW16" style="color: var(--tertiary-color)">NickW16</a>.</p>
+                                `
+                                ;                       
 };
 
 // welcome message
@@ -34,3 +45,29 @@ dashboardClear.addEventListener('click', () => {
                              <div id="main-page-message"></div>`;
     renderMainPageMessage();
 });
+
+//user theme changes
+
+const themeSelect = document.getElementById("theme-select");
+
+// event listener when user changes theme
+themeSelect.addEventListener("change", (e) => {
+    const selectedTheme = e.target.value;
+  
+    // remove all theme classes
+    document.body.classList.remove("light-theme", "dark-theme", "blue-theme", "pink-theme", "red-theme");
+  
+    // add selected theme class
+    document.body.classList.add(`${selectedTheme}-theme`);
+  
+    // save preference to local storage
+    localStorage.setItem("theme", selectedTheme);
+  });
+
+// apply saved theme
+window.addEventListener("DOMContentLoaded", () => {
+    const savedTheme = localStorage.getItem("theme") || "light";
+  
+    document.body.classList.add(`${savedTheme}-theme`);
+    themeSelect.value = savedTheme;
+  });
